af8d4fd902ac3bf74da76353c18c0967554f6a96
mlem <33568957+meow2000@users.noreply.github.com>
2021-11-18 16:32:11 +0700

    Add files via upload

---
+++ lab5/Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Handle.php" method="POST">
+        <label>Title: </label>
+        <input type="text" name="title" value="">
+        <br>
+        <label>Year: </label>
+        <input type="text" name="year" value="">
+        <br>
+        <label>Copyright: </label>
+        <input type="text" name="copyright" value="">
+        <br>
+        <label>Content: </label>
+        <textarea name="content" rows="8" cols="80"></textarea>
+        <br>
+        <input type="submit" name="" value="SUBMIT">
+    </form>
+</body>
+</html>
